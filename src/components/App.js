598afe32332191899e0d73ce9c94600fccd1c60c
Ernest Roca Vivó <khenblack@gmail.com>
2019-01-03 11:37:39 +0100

    Create Store with context

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,21 @@
 import React, { Component } from 'react';
 import UserCreate from './UserCreate';
-import LanguageContext from '../contexts/LanguageContext';
+import LanguageSelector from './LanguageSelector';
+import { LanguageStore } from '../contexts/LanguageContext';
 import ColorContext from '../contexts/ColorContext';
 
 class App extends Component {
-  state = { language: 'english' }
-
-  onLanguageChange = language => {
-    this.setState({ language });
-  }
 
   render() {
     return (
       <div className='ui container'>
-        <div>
-          Select a language:
-          <i className='flag us' onClick={() => this.onLanguageChange('english')} />
-          <i className='flag nl' onClick={() => this.onLanguageChange('dutch')} />
-        </div>
-        <ColorContext.Provider value={{ color: 'primary' }}>
-          <LanguageContext.Provider value={{ language: this.state.language }}>
+        <LanguageStore>
+          <LanguageSelector />
+          <ColorContext.Provider value={{ color: 'primary' }}>
             <UserCreate />
-          </LanguageContext.Provider>
-        </ColorContext.Provider>
-      </div>
+          </ColorContext.Provider>
+        </LanguageStore>
+      </div >
     );
   }
 }
